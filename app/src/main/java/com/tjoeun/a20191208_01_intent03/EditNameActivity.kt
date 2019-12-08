c509c3664c9bb26b6c09773575c31cd0f29c4ffe
미현 김 <rlaalgus132@naver.com>
2019-12-08 10:12:12 +0900

    입력 화면 그리기

---
+++ app/src/main/java/com/tjoeun/a20191208_01_intent03/EditNameActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191208_01_intent03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_name)
+    }
+}
